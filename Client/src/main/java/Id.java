c8b0e3491b923f1638b3402e80722de970036b3a
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-21 12:26:48 -0400

    setup Id class

--- Client/src/main/java/Id.java
+++ Client/src/main/java/Id.java
@@ -1,2 +1,36 @@
 public class Id {
+
+    private String userId;
+    private String name;
+    private String githubId;
+
+    public Id(String userId, String name, String githubId) {
+        this.userId = userId;
+        this.name = name;
+        this.githubId = githubId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithubId() {
+        return githubId;
+    }
+
+    public void setGithubId(String githubId) {
+        this.githubId = githubId;
+    }
 }
